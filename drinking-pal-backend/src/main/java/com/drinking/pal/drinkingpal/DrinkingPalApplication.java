b44ed00887582d07ae93e911cf29622595e8df71
Youssef ABIDI <jobs.youssef@gmail.com>
2021-11-13 12:10:07 +0100

    initialized backend project

---
+++ drinking-pal-backend/src/main/java/com/drinking/pal/drinkingpal/DrinkingPalApplication.java
@@ -0,0 +1,13 @@
+package com.drinking.pal.drinkingpal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrinkingPalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DrinkingPalApplication.class, args);
+    }
+
+}
---
+++ drinking-pal-backend/src/test/java/com/drinking/pal/drinkingpal/DrinkingPalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drinking.pal.drinkingpal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrinkingPalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
